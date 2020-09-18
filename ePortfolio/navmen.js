b51c810977f754d937c218d9a97831c771c66d61
Lademirion <60258311+Lademirion@users.noreply.github.com>
2020-09-19 01:52:04 +0200

    Added eportfolio

---
+++ ePortfolio/navmen.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mainsidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mainsidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
